346c2d9d402b1bea674d1d51ae4e3822eec7600d
KlynLeo <153291077+KlynLeo@users.noreply.github.com>
2025-03-07 13:42:31 +0200

    final

--- Lab2/tema/src/Solution.java
+++ Lab2/tema/src/Solution.java
@@ -1,3 +1,25 @@
-public class Solution { /** Leonte Calin **/
+public class Solution { //Leonte Calin
+    public static void allocateProjects(Problem problem) {
+        boolean[] allocatedProjects = new boolean[problem.projects.length];
+        String[] allocations = new String[problem.students.length];
 
+        for (int i = 0; i < problem.students.length; i++) {
+            for (Project preferredProject : problem.students[i].availableProjects) {
+                for (int j = 0; j < problem.projects.length; j++) {
+                    if (problem.projects[j].equals(preferredProject) && !allocatedProjects[j]) {
+                        allocations[i] = problem.students[i].name + " va face proiectul " + problem.projects[j].title;
+                        allocatedProjects[j] = true;
+                        break;
+                    }
+                }
+                if (allocations[i] != null) break;
+            }
+        }
+
+        for (String allocation : allocations) {
+            if (allocation != null)
+                System.out.println(allocation);
+        }
+    }
 }
+
