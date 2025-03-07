71392ccea2844c23aa011626235c49b407a86ec3
KlynLeo <153291077+KlynLeo@users.noreply.github.com>
2025-03-07 13:43:10 +0200

    final

--- Lab2/tema/src/Teacher.java
+++ Lab2/tema/src/Teacher.java
@@ -1,10 +1,18 @@
+import java.util.Arrays;
+import java.util.Date;
 
 public class Teacher extends Person { /** Leonte Calin **/
     private Project[] proposedProjects;
 
-    public Teacher(String name, Project[] projects) {
-        super(name);
+    public Teacher(String name, String birthDate, Project[] projects) {
+        super(name, birthDate);
         proposedProjects = projects;
     }
 
+    public boolean equals(Object obj) {
+        if (!super.equals(obj))
+            return false;
+        Teacher teacher = (Teacher) obj;
+        return Arrays.equals(((Teacher) obj).proposedProjects, proposedProjects);
+    }
 }
