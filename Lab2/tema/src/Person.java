a4636eafb8b202c06d1b9014051b1542c6635132
KlynLeo <153291077+KlynLeo@users.noreply.github.com>
2025-03-07 13:39:41 +0200

    final

--- Lab2/tema/src/Person.java
+++ Lab2/tema/src/Person.java
@@ -3,17 +3,18 @@ import java.util.Objects;
 
 public class Person { /** Leonte Calin **/
     protected String name;
-    protected Date birthDate;
+    protected String birthDate;
 
-    public Person (String name){
+    public Person (String name, String birthDate){
         this.name = name;
+        this.birthDate = birthDate;
     }
 
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
-        if (obj == null || getClass() != obj.getClass())
+        if (getClass() != obj.getClass())
             return false;
         Person person = (Person) obj;
         return Objects.equals(name, person.name) && Objects.equals(birthDate, person.birthDate);
