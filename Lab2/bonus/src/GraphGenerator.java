34b160e5016a9a5abdd5aebcfa3376e69b5b363a
KlynLeo <153291077+KlynLeo@users.noreply.github.com>
2025-03-07 14:00:16 +0200

    final

--- Lab2/bonus/src/GraphGenerator.java
+++ Lab2/bonus/src/GraphGenerator.java
@@ -1,1 +1,21 @@
+import java.util.*;
+
+public class GraphGenerator {
+
+    public static Map<Integer, Set<Integer>> generateGraph(int numStudents, int numProjects, int maxChoicesPerStudent) {
+        Random rand = new Random();
+        Map<Integer, Set<Integer>> studentPreferences = new HashMap<>();
+
+        for (int student = 0; student < numStudents; student++) {
+            int numOfChoices = rand.nextInt(maxChoicesPerStudent) + 1;
+            Set<Integer> projects = new HashSet<>();
+            while (projects.size() < numOfChoices) {
+                projects.add(rand.nextInt(numProjects));
+            }
+            studentPreferences.put(student, projects);
+        }
+        return studentPreferences;
+    }
+}
+
 
