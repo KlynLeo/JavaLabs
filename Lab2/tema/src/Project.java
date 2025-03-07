1cfc3c564ec23a90cb3d83f75343b950a9a5a784
KlynLeo <153291077+KlynLeo@users.noreply.github.com>
2025-03-07 13:40:07 +0200

    final

--- Lab2/tema/src/Project.java
+++ Lab2/tema/src/Project.java
@@ -1,21 +1,21 @@
 import java.util.Objects;
 
 public class Project { /** Leonte Calin **/
-    private String title;
+    String title;
     private Type type;
-    private Teacher proposer;
 
     public Project (String title, Type type)
     {
-        this.type = type;
         this.title = title;
+        this.type = type;
+
     }
 
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+        if (!(obj instanceof Project)) return false;
         Project project = (Project) obj;
-        return Objects.equals(project.title, title) && Objects.equals(project.proposer, proposer);
+        return Objects.equals(project.title, title) && Objects.equals(project.type, type);
     }
 }
